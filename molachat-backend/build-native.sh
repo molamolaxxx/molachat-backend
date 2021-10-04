ec386be1d2f454f0def5b78da4a11fc16b3dd5b1
molamolaxxx <408287749@qq.com>
2021-10-05 03:19:42 +0800

    fix

--- molachat-backend/build-native.sh
+++
@@ -1,2 +0,0 @@
-echo "start building native application"
-mvn -Pnative-image package -f pom-native.xml
--- molachat-backend/src/main/resources/static/js/app/mail.js
+++
@@ -1,6 +0,0 @@
-// 欢迎页逻辑
-$(document).on('click', '.folder', function() {
-    setTimeout(() => {
-        $(".swal-overlay")[0].click()
-    },500)
-  });
