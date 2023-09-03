725b690bc262b6ee2863ec294b427ce3458ebacf
molamolaxxx <408287749@qq.com>
2023-09-03 20:00:03 +0800

    优化

--- src/main/java/com/mola/molachat/robot/handler/impl/CmdProxyRobotHandler.java
+++ src/main/java/com/mola/molachat/robot/handler/impl/CmdProxyRobotHandler.java
@@ -5,6 +5,8 @@ import com.mola.cmd.proxy.client.consumer.CmdSender;
 import com.mola.cmd.proxy.client.resp.CmdInvokeResponse;
 import com.mola.cmd.proxy.client.resp.CmdResponseContent;
 import com.mola.molachat.entity.Message;
+import com.mola.molachat.event.action.BaseAction;
+import com.mola.molachat.robot.action.EmptyAction;
 import com.mola.molachat.robot.action.MessageSendAction;
 import com.mola.molachat.robot.event.BaseRobotEvent;
 import com.mola.molachat.robot.event.MessageReceiveEvent;
@@ -24,9 +26,10 @@ import java.util.Arrays;
  **/
 @Component
 @Slf4j
-public class CmdProxyRobotHandler implements IRobotEventHandler<MessageReceiveEvent, MessageSendAction> {
+public class CmdProxyRobotHandler implements IRobotEventHandler<MessageReceiveEvent, BaseAction> {
+
     @Override
-    public MessageSendAction handler(MessageReceiveEvent baseEvent) {
+    public BaseAction handler(MessageReceiveEvent baseEvent) {
         MessageSendAction messageSendAction = new MessageSendAction();
         try {
             Message message = baseEvent.getMessage();
@@ -41,13 +44,20 @@ public class CmdProxyRobotHandler implements IRobotEventHandler<MessageReceiveEv
             CmdInvokeResponse<CmdResponseContent> response = CmdSender.INSTANCE
                     .send(splitRes[0], message.getSessionId(), args);
             Assert.notNull(response, "response is null");
-            Assert.isTrue(response.isSuccess(), response.getMsg());
+            if (!response.isSuccess()) {
+                log.error("RemoteRobotChatHandler not success " + JSONObject.toJSONString(baseEvent) + ";" + response.getMsg());
+                EmptyAction emptyAction = new EmptyAction();
+                emptyAction.setSkip(true);
+                return emptyAction;
+            }
             Assert.notNull(response.getData(), "data is null");
             Assert.notNull(response.getData().getResultMap(), "resultMap is null");
+
             String result = response.getData().getResultMap().get("result");
             if (StringUtils.isBlank(result)) {
-                messageSendAction.setResponsesText("执行结果为空");
-                messageSendAction.setFinalExec(Boolean.TRUE);
+                EmptyAction emptyAction = new EmptyAction();
+                emptyAction.setFinalExec(Boolean.TRUE);
+                return emptyAction;
             }
 
             messageSendAction.setResponsesText(result);
