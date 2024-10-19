963002ae36df6d98aa4100ba6e49b3b41f1249cd
molamolaxxx <408287749@qq.com>
2024-10-20 01:13:08 +0800

    1、远程命令help适配

--- src/main/java/com/mola/molachat/robot/handler/impl/cmd/HelpCmdHandler.java
+++ src/main/java/com/mola/molachat/robot/handler/impl/cmd/HelpCmdHandler.java
@@ -1,11 +1,16 @@
 package com.mola.molachat.robot.handler.impl.cmd;
 
+import com.alibaba.nacos.common.utils.MapUtils;
+import com.mola.cmd.proxy.client.consumer.CmdSender;
 import com.mola.molachat.robot.event.CommandInputEvent;
 import com.mola.molachat.robot.handler.impl.BaseCmdRobotHandler;
+import com.mola.molachat.session.model.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author : molamola
@@ -14,6 +19,7 @@ import java.util.List;
  * @date : 2022-09-12 16:26
  **/
 @Component
+@Slf4j
 public class HelpCmdHandler extends BaseCmdRobotHandler {
 
     @Resource
@@ -28,6 +34,18 @@ public class HelpCmdHandler extends BaseCmdRobotHandler {
     protected String executeCommand(CommandInputEvent baseEvent) {
         try {
             StringBuilder stringBuilder = new StringBuilder();
+
+            Message message = baseEvent.getMessageReceiveEvent().getMessage();
+            Map<String, String> remoteCmdDescMap = CmdSender.INSTANCE.fetchDescriptionMap(message.getSessionId());
+            if (MapUtils.isNotEmpty(remoteCmdDescMap)) {
+                stringBuilder.append("------ remote command help ------").append("\n");
+                remoteCmdDescMap.forEach((cmd, desc) -> {
+                    stringBuilder.append(String.format("【%s】: %s", cmd, desc));
+                    stringBuilder.append("\n");
+                });
+            }
+
+            stringBuilder.append("------ local command help ------").append("\n");
             for (int i = 0; i < cmdRobotHandlers.size(); i++) {
                 BaseCmdRobotHandler cmdRobotHandler = cmdRobotHandlers.get(i);
                 stringBuilder.append(String.format("【%s】: %s", cmdRobotHandler.getCommand(), cmdRobotHandler.getDesc()));
@@ -35,6 +53,7 @@ public class HelpCmdHandler extends BaseCmdRobotHandler {
             }
             return stringBuilder.toString();
         } catch (Exception e) {
+            log.error("help exec failed", e);
             throw new RuntimeException(e);
         }
     }
